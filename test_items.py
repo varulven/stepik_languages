b7e5a898eec49fd0f813abf98edac8cc14d5ac91
yshenshteyn <vanapagan@list.ru>
2020-03-12 10:20:07 +0300

    test_items.py file version 2.0 with assert only for fr language and with function

--- test_items.py
+++ test_items.py
@@ -1,12 +1,12 @@
 import time
 import os
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#def test_input_text(expected_result, actual_result):
-#    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+def input_text(expected_result, actual_result):
+    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
  
 def test_button_add_to_basket_language(browser):
     browser.get(link)
     time.sleep(30)
-    e = browser.find_element_by_css_selector(".btn-add-to-basket")
-    assert "Ajouter au panier" == e.text,f"expected Ajouter au panier, got {e.text}"
+    button_name = browser.find_element_by_css_selector(".btn-add-to-basket")
+    input_text("Ajouter au panier",button_name.text)
     
